04fe3cf1c95beb275274e91f2c85795a60116659
veoco <one@nomox.cn>
2022-03-22 14:15:06 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-tailwindcss",
+    version="0.1.0",
+    author="Veoco",
+    author_email="one@nomox.cn",
+    description="Develop with Tailwind CSS in Django flavour.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/veoco/django-tailwindcss",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

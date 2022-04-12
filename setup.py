a287c444dc6ca87ee21336e1082271f88b49513e
veoco <one@nomox.cn>
2022-04-12 12:09:32 +0800

    upgrade to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-tailwindcss",
-    version="0.2.2",
+    version="0.3.0",
     author="Veoco",
     author_email="one@nomox.cn",
     description="Develop with Tailwind CSS in Django flavour.",

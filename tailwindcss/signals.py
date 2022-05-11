5d1585ba6236b78bd8f8e2962e9e6a6cfe2ee7a5
veoco <one@nomox.cn>
2022-05-11 21:05:16 +0800

    fix file change refresh

--- tailwindcss/signals.py
+++ tailwindcss/signals.py
@@ -1,5 +1,14 @@
+from django.core.cache import cache
+
 from tailwindcss.templatetags.tailwindcss import tailwind
 
 
 def refresh_tailwindcss(sender, **kwargs):
-    tailwind.refresh()
+    path = kwargs.get('file_path')
+    cache_key = 'tw/' + path
+
+    tailwindcss = cache.get(cache_key)
+    if tailwindcss:
+        tailwind.refresh(path)
+    else:
+        tailwind.refresh()
